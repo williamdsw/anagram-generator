af7282b83ca9c04fc07a5d7d715618c5e73e8860
William <williamdsw@outlook.com>
2020-05-01 13:46:34 -0300

    Sintax refactors for all files

--- js/anagram-generator.js
+++ js/anagram-generator.js
@@ -5,13 +5,12 @@
 
 "use strict";
 
-//---------------------------------------------------------------------------------------//
-// PARAMETERS
+// VARIABLES
 
 // Parameters
 let anagrams = [];
-let rejected = 0;
-let intervalID = null;
+let numberOfRejected = 0;
+let intervalId = null;
 
 // Elements
 let btnGenerator = null;
@@ -19,57 +18,55 @@ let spanProgress = null;
 let btnStop = null;
 let divOutput = null;
 let pRejected = null;
+let inputWord = null;
 
-//---------------------------------------------------------------------------------------//
 // HELPER FUNCTIONS
 
-window.addEventListener ("load", function ()
-{
-    spanProgress = document.querySelector ("#progress span");
-    btnStop = document.getElementById ("stop");
-    divOutput = document.getElementById ("output");
-    pRejected = document.getElementById ("rejected");
-    let input = document.getElementById ("word");
-    btnGenerator = document.getElementById ("generator");
+window.addEventListener ('load', function () {
+    spanProgress = document.querySelector ('#pProgress span');
+    btnStop = document.getElementById ('buttonStop');
+    divOutput = document.getElementById ('divOutput');
+    pRejected = document.getElementById ('pRejected');
+    inputWord = document.getElementById ('inputWord');
+    btnGenerator = document.getElementById ('btnGenerator');
 
     // EVENTS
 
-    input.addEventListener ("keyup", function (e)
-    {
-        spanProgress.textContent = (this.value.length > 1 ? "Click on 'GENERATE'" : "Two or more letters (words)");
-        divOutput.innerHTML = "";
-        btnStop.style.display = "none";
+    inputWord.addEventListener ('keyup', function () {
+        spanProgress.textContent = (this.value.length > 1 ? `Click on 'GENERATE'` : 'Two or more letters (words)');
+        divOutput.innerHTML = '';
+        btnStop.style.display = 'none';
         btnGenerator.disabled = false;
 
-        if (typeof (intervalID) === "number") { clearInterval (intervalID); }
-    });
-
-    btnGenerator.addEventListener ("click", function (e)
-    {
-        if (input.value.length > 1) 
-        {
-            generateAnagrams (input.value);
-            this.disabled = true;
-            divOutput.innerHTML = "";
+        if (typeof (intervalId) === 'number') {
+            clearInterval (intervalId); 
         }
     });
 
-    btnStop.addEventListener ("click", function (e)
-    {
-        if (typeof (intervalID) === "number") 
-        {
-            clearInterval (intervalID);
-            this.style = "none";
-            rejected = 0;
+    btnGenerator.addEventListener ('click', function () {
+        if (inputWord.value.length > 1) {
+            generateAnagrams (inputWord.value);
+            this.disabled = true;
+            divOutput.innerHTML = '';
+        }
+    });
+
+    btnStop.addEventListener ('click', function () {
+        if (typeof (intervalId) === 'number') {
+            clearInterval (intervalId);
+            this.style = 'none';
+            numberOfRejected = 0;
             anagrams = [];
             btnGenerator.disabled = false;
         }
     });
 });
 
-function factorial (number)
-{
-    if (number === 1) { return 1; }
+function factorial (number) {
+    if (number === 1) { 
+        return 1; 
+    }
+
     return number * factorial (number - 1);
 }
 
@@ -77,122 +74,109 @@ function factorial (number)
  * Generates the number of max ocorrences based on word
  * @param {String} word 
  */
-function generateMaxOcorrences (word)
-{
+function generateMaxOcorrences (word) {
     let maxOcorrences = 1;
 
-    word = word.normalize ("NFD");
-    word = word.replace (/[\u0300-\u036f]/g, "");
-    let letters = word.split ("");
+    word = word.normalize ('NFD');
+    word = word.replace (/[\u0300-\u036f]/g, '');
+    const LETTERS = word.split ('');
     let repeated = {};
 
     // Passing letters to object
-    letters.map ((item) => 
-    {
-        if (item !== "")
-        {
-            let ocorrence = letters.filter ((letter) => letter === item).length;
-            repeated[item] = ocorrence;
+    LETTERS.map ((char) => {
+        if (char !== '') {
+            let ocorrence = LETTERS.filter ((c) => c === char).length;
+            repeated[char] = ocorrence;
         }
     });
 
     // Calculates
-    if (Object.keys (repeated).length > 1)
-    {
-        let values = Object.values (repeated);
+    if (Object.keys (repeated).length > 1) {
+        const VALUES = Object.values (repeated);
         let totalRepeated = 1;
-        values.map ((item, index) =>  { totalRepeated *= factorial (item); });
-        maxOcorrences = factorial (word.length) / totalRepeated;
+        VALUES.map ((item, index) => totalRepeated *= factorial (item));
+        maxOcorrences = (factorial (word.length) / totalRepeated);
     }
     
     return maxOcorrences;
 }
 
-/**
- * Generates anagrams based on the word passed
- * @param {String} word 
- */
-function generateAnagrams (word)
-{
+// Generates anagrams based on the word passed
+function generateAnagrams (word) {
     anagrams = [];
-    let start = Date.now ();
+    const START = Date.now ();
 
     // Initial parameters
-    let letters = word.split ("");
-    let maxOcorrences = generateMaxOcorrences (word);
+    const LETTERS = word.split ('');
+    const MAX_OCORRENCES = generateMaxOcorrences (word);
     let arrInt = [];
 
     // Fills the array of integers
-    letters.map ((item, index) => { arrInt.push (index); });
+    LETTERS.map ((item, index) => arrInt.push (index));
 
     // Shows the sop button
-    btnStop.style.display = "inline-block";
+    btnStop.style.display = 'inline-block';
 
     let index = 0;
-    intervalID = setInterval (function ()
-    {
+    intervalId = setInterval (function () {
         let newWord = [];
         let newIntegers = [];
 
-        for (let j = 0; j < letters.length; j++) 
-        {
-            if (newWord.length === letters.length) { break; }
+        for (let j = 0; j < LETTERS.length; j++) {
+            if (newWord.length === LETTERS.length) {
+                break; 
+            }
 
             // Generates random index e get value / integer from arrays
-            let randomIndex = Math.ceil (Math.random () * letters.length) - 1;
-            let letter = letters[randomIndex];
-            let integer = arrInt[randomIndex];
+            const RANDOM_INDEX = Math.ceil (Math.random () * LETTERS.length) - 1;
+            let letter = LETTERS[RANDOM_INDEX];
+            let integer = arrInt[RANDOM_INDEX];
 
-            if (newIntegers.indexOf (integer) === -1) 
-            {
+            if (newIntegers.indexOf (integer) === -1) {
                 newIntegers.push (integer);
                 newWord.push (letter);
             }
-            else { j--; }
+            else { 
+                j--; 
+            }
         }
 
-        newWord = newWord.join ("");
+        newWord = newWord.join ('');
 
         // Inserts into anagrams and renders it to screen
-        if (anagrams.indexOf (newWord) == -1)
-        {
+        if (anagrams.indexOf (newWord) == -1) {
             anagrams.push (newWord);
             renderElement (word, newWord, divOutput);
-            spanProgress.innerHTML = `Generated <b> ${anagrams.length} </b> of possible ${maxOcorrences} anagrams...`;
+            spanProgress.innerHTML = `Generated <b> ${anagrams.length} </b> of possible ${MAX_OCORRENCES} anagrams...`;
             index++;
         }
-        else
-        {
-            rejected++;
-            pRejected.innerHTML = `<span> Duplicates Rejected : <b> ${rejected} </b> </span>`
+        else {
+            numberOfRejected++;
+            pRejected.innerHTML = `<span> Duplicates Rejected : <b> ${numberOfRejected} </b> </span>`
             index--;
         }
         
         // Breaks
-        if (anagrams.length === maxOcorrences) { index = maxOcorrences; }
-        if (index === maxOcorrences)
-        {
-            clearInterval (intervalID);
-            btnStop.style.display = "none";
-            var finished = Date.now ();
-            spanProgress.innerHTML = `Generated ${anagrams.length} anagram${anagrams.length > 1 ? "s" : ""} in <b> ${finished - start} ms </b>!`;
-            rejected = 0;
+        if (anagrams.length === MAX_OCORRENCES) {
+            index = MAX_OCORRENCES; 
+        }
+
+        if (index === MAX_OCORRENCES) {
+            clearInterval (intervalId);
+            btnStop.style.display = 'none';
+            const FINISHED = Date.now ();
+            spanProgress.innerHTML = `Generated ${anagrams.length} anagram${anagrams.length > 1 ? "s" : ""} in <b> ${FINISHED - START} ms </b>!`;
+            numberOfRejected = 0;
             anagrams = [];
             btnGenerator.disabled = false;
         }
     });
 }
 
-/**
- * Create a new paragraph and appends to div
- * @param {String} word
- * @param {String} newWord
- * @param {Element} parent 
- */
-function renderElement (word, newWord, parent)
-{
-    let paragraph = document.createElement ("p");
-    paragraph.setAttribute ("class", (newWord === word ? "result same" : "result"));
+// Create a new paragraph based on word and appends to div
+function renderElement (word, newWord, parent) {
+    let paragraph = document.createElement ('p');
+    paragraph.setAttribute ('class', (newWord === word ? 'result same' : 'result'));
     paragraph.textContent = newWord;
     parent.appendChild (paragraph);
 }
